8ab2333f58bf8a74846fb90b60623f6cd5577d9a
Brian <brian.montenegro@techhigh.us>
2017-12-11 08:03:43 -0500

    Initial commit

---
+++ TestClass.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+/**
+ * Write a description of class TestClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class TestClass
+{
+   public static void main(String args[]){
+       DrawingPanel panel = new DrawingPanel(800,800);
+       Graphics g = panel.getGraphics();
+       
+       Dragon head = new Dragon();
+       
+       head.drawDragon(g);
+     
+       System.out.println(head);
+       
+       Dragon body = new Dragon(300,300,2,Color.GREEN,25);
+       body.drawDragon(g);
+    }
+}
