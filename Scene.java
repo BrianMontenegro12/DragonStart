7d999165242d94aa5507cd3b9b27d16601b37024
Brian <brian.montenegro@techhigh.us>
2017-12-18 08:04:48 -0500

    scene stars

--- Scene.java
+++ Scene.java
@@ -20,6 +20,11 @@ public class Scene
 {
     // instance variables - replace the example below with your own
     private int width;
+    private int size;
+    private int x;
+    private int y;
+    private int f;
+    private int l;
 
     /**
      * Constructor for objects of class Scene
@@ -28,22 +33,54 @@ public class Scene
     {
         // initialise instance variables
         width = 0;
+        size = 5;
+        x = 275;
+        y = 50;
+        f = 5;
+        l = 5;
     }
     public void drawBackground(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(0,0,800,800);
         g.setColor(Color.WHITE);
-        g.fillOval(500,80,5,5);
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * l,5,5);
     }
-
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 2),5,5);
+    }
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 3),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 4),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 5),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 6),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 7),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 8),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 9),5,5);
+}
+        for(int f = 5; f <= 75; f += 5){
+            g.fillOval(x + size * f, y + size * (l * 10),5,5);
+}
+}
     /**
      * An example of a method - replace this comment with your own
      *
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
+    public int sampleMethod(){
         // put your code here
         return width;
     }
